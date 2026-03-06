c48be44c1b212f2db9a3c9fe81e8ffb26a9fd396
John Curley <johndanielcurley@gmail.com>
2026-03-06 17:55:56 +1300

    chore: v3.0.12 - finalize GLU Integrity Audit: perform full-stack verification and synchronize bit-widths in quadray_rotor.v

--- hardware/rtl/quadray_rotor.v
+++ hardware/rtl/quadray_rotor.v
@@ -14,10 +14,10 @@ module quadray_rotor (
     // We instantiate 4x SQR-ALU units to scale the radials simultaneously.
     wire [63:0] m1, m2, m3, m4;
     
-    spu_smul rotor_scale_1 (.a1(q1_in[31:0]), .b1(q1_in[63:32]), .a2(surd_ratio[15:0]), .b2(surd_ratio[31:16]), .res_a(m1[31:0]), .res_b(m1[63:32]));
-    spu_smul rotor_scale_2 (.a1(q2_in[31:0]), .b1(q2_in[63:32]), .a2(surd_ratio[15:0]), .b2(surd_ratio[31:16]), .res_a(m2[31:0]), .res_b(m2[63:32]));
-    spu_smul rotor_scale_3 (.a1(q3_in[31:0]), .b1(q3_in[63:32]), .a2(surd_ratio[15:0]), .b2(surd_ratio[31:16]), .res_a(m3[31:0]), .res_b(m3[63:32]));
-    spu_smul rotor_scale_4 (.a1(q4_in[31:0]), .b1(q4_in[63:32]), .a2(surd_ratio[15:0]), .b2(surd_ratio[31:16]), .res_a(m4[31:0]), .res_b(m4[63:32]));
+    spu_smul rotor_scale_1 (.a1(q1_in[31:0]), .b1(q1_in[63:32]), .a2(surd_ratio[31:0]), .b2(surd_ratio[63:32]), .res_a(m1[31:0]), .res_b(m1[63:32]));
+    spu_smul rotor_scale_2 (.a1(q2_in[31:0]), .b1(q2_in[63:32]), .a2(surd_ratio[31:0]), .b2(surd_ratio[63:32]), .res_a(m2[31:0]), .res_b(m2[63:32]));
+    spu_smul rotor_scale_3 (.a1(q3_in[31:0]), .b1(q3_in[63:32]), .a2(surd_ratio[31:0]), .b2(surd_ratio[63:32]), .res_a(m3[31:0]), .res_b(m3[63:32]));
+    spu_smul rotor_scale_4 (.a1(q4_in[31:0]), .b1(q4_in[63:32]), .a2(surd_ratio[31:0]), .b2(surd_ratio[63:32]), .res_a(m4[31:0]), .res_b(m4[63:32]));
 
     // 2. Tetrahedral Tiling (60-Degree Shuffles)
     // The 'Rotation' is achieved by permuting the scaled radials.
