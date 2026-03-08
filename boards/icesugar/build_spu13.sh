d07d2c0c4eba45c89e3081f7e768c99cc4822814
John Curley <johndanielcurley@gmail.com>
2026-03-09 10:36:06 +1300

    Synthesis Refinement (v3.3.47): universal Makefile and reification script for iCeSugar

--- boards/icesugar/build_spu13.sh
+++ boards/icesugar/build_spu13.sh
@@ -1,33 +1,56 @@
 #!/bin/bash
-# SPU-13 Reification Script: Phase 1 (Resonant Anchor) (v3.3.7)
+# SPU-13 Reification Script: Universal Build (v3.3.47)
 # Targets: iCE40UP5K (iCeSugar)
 
-echo "--- Initializing SPU-13 Manifold Synthesis ---"
+# 1. Configuration
+TOP=${1:-spu13_top} # Default to minimal anchor if no arg provided
+PROJ="spu13_icesugar"
+DEVICE="up5k"
+PACKAGE="sg48"
+RTL_DIR="../../rtl"
 
-# 1. Synthesis (Yosys) - Creating the Logic Manifold
-yosys -p "synth_ice40 -top spu13_top -json spu13.json" \
-    spu13_top.v \
-    ../../rtl/spu_fractal_clk.v \
-    ../../rtl/spu_coherence_ecc.v \
-    ../../rtl/spu_rational_trig.v \
-    ../../rtl/spu_geometry_fluidizer.v \
-    ../../rtl/spu_gram_controller.v \
-    ../../rtl/spu_fluid_solver.v \
-    ../../rtl/spu_annealer.v \
-    ../../rtl/spu_tensegrity_balancer.v \
-    ../../rtl/spu_harmonic_vis.v \
-    ../../rtl/spu_identity_monad.v \
-    ../../rtl/spu_fractal_bypass.v \
-    ../../rtl/spu_harmonic_handshake.v
+if [ "$TOP" == "icesugar_full_manifold" ]; then
+    PCF="spu13_icesugar_full.pcf"
+else
+    PCF="spu13_icesugar_v2.pcf"
+fi
 
-# 2. Place & Route (nextpnr) - Mapping the IVM Geometry
-# Using --force to allow for Virtual Induction 'unconnected' pins
-nextpnr-ice40 --up5k --package sg48 --json spu13.json --pcf spu13_icesugar_v2.pcf --asc spu13.asc
+echo "--- Initializing SPU-13 Manifold Synthesis: $TOP ---"
 
-# 3. Bitstream Timing Check - Verifying Null Hysteresis
-icetime -d up5k -P sg48 -t spu13.asc
+# 2. Source List
+SRC="$TOP.v \
+    $RTL_DIR/spu_fractal_clk.v \
+    $RTL_DIR/spu_harmonic_handshake.v \
+    $RTL_DIR/spu_coherence_ecc.v \
+    $RTL_DIR/spu_core.v \
+    $RTL_DIR/spu_ecc.v \
+    $RTL_DIR/spu_permute.v \
+    $RTL_DIR/spu_permute_13.v \
+    $RTL_DIR/spu_smul_13.v \
+    $RTL_DIR/spu_rational_trig.v \
+    $RTL_DIR/spu_geometry_fluidizer.v \
+    $RTL_DIR/spu_gram_controller.v \
+    $RTL_DIR/spu_fluid_solver.v \
+    $RTL_DIR/spu_annealer.v \
+    $RTL_DIR/spu_tensegrity_balancer.v \
+    $RTL_DIR/spu_harmonic_vis.v \
+    $RTL_DIR/spu_identity_monad.v \
+    $RTL_DIR/spu_fractal_bypass.v \
+    $RTL_DIR/spu_self_test.v \
+    $RTL_DIR/spu_io_bridge.v \
+    $RTL_DIR/surd_uart_tx.v \
+    $RTL_DIR/surd_multiplier.v"
 
-# 4. Packaging - Generating the Reified Bitstream
-icepack spu13.asc spu13.bin
+# 3. Synthesis (Yosys)
+yosys -p "synth_ice40 -top $TOP -json $PROJ.json" $SRC
 
-echo "--- Build Complete: spu13.bin is ready for the Primer Phase ---"
+# 4. Place & Route (nextpnr)
+nextpnr-ice40 --$DEVICE --package $PACKAGE --json $PROJ.json --pcf $PCF --asc $PROJ.asc
+
+# 5. Timing Audit (icetime)
+icetime -d $DEVICE -P $PACKAGE -t $PROJ.asc
+
+# 6. Packaging (icepack)
+icepack $PROJ.asc $PROJ.bin
+
+echo "--- Build Complete: $PROJ.bin is ready for Reification ---"
